98c2604b97df7375092e9c0d4fac19095b13de6a
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-12-07 16:48:18 +0530

    Add files via upload

---
+++ Square/main.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().setup(400,400)
+turtle.Screen().bgcolor("yellow")
+pen = turtle.Turtle()
+
+for i in range(0,4):
+    pen.forward(100)
+    pen.left(90)
+
+turtle.done
